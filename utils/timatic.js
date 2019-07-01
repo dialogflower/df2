226e80553b5d6ea5e5abeec3542273417d551b9a
dialogflower <dialogflowers@gmail.com>
2019-07-01 12:42:50 +0300

    Minor visual fix for text output

--- utils/timatic.js
+++ utils/timatic.js
@@ -28,7 +28,7 @@ desitination += randomCountryTwo.alpha2;
 const itinerary = `Let's find visa requirements for a national of ` + randomCountryOne.name + ` travelling to ` + randomCountryTwo.name;
 const query = endpoint + '?' + prefix + '&' + passtype + '&' + nationality + '&' + residence + '&' + desitination + '&' + postfix;
 // console.log(query);
-console.log(itinerary + '=================================================================================\n');
+console.log(itinerary + '\n=================================================================================\n');
 let advice;
 
 rp(query)
